5bf477a35222d787ac4e3f7e1fb083f8bd8312ae
erickcharlesneves <erick.neves.projetos@gmail.com>
2020-11-30 17:01:17 -0300

    Exercicios da aula9 em javascript

---
+++ semana-3/variaveis/js-vanilla-template/srcQST3/index.js
@@ -0,0 +1,20 @@
+
+
+//PARTE2
+
+//QST3
+let comida = ["açai", "chocolate", "uva", "morango", "picanha"]
+
+console.log (comida[0], comida[1], comida[2], comida[3], comida[4])
+
+console.log("Essas são as minhas comidas preferidas: ")
+console.log(comida[0])
+console.log(comida[1])
+console.log(comida[2])
+console.log(comida[3])
+console.log(comida[4])
+
+//Aqui vai um desafio: pergunte ao usuário uma comida preferida. 
+//Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima na tela a nova lista 
+
+
