dfd4b6c7361aaa69537dbfe29be0c276732bed4f
erickcharlesneves <erick.neves.projetos@gmail.com>
2020-11-30 23:28:48 -0300

    Exercicio 4 que faltou terminar e 3c

--- semana-3/variaveis/js-vanilla-template/srcQST3/index.js
+++ semana-3/variaveis/js-vanilla-template/srcQST3/index.js
@@ -7,6 +7,8 @@ let comida = ["açai", "chocolate", "uva", "morango", "picanha"]
 
 console.log (comida[0], comida[1], comida[2], comida[3], comida[4])
 
+console.log (comida)
+
 console.log("Essas são as minhas comidas preferidas: ")
 console.log(comida[0])
 console.log(comida[1])
@@ -18,3 +20,13 @@ console.log(comida[4])
 //Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima na tela a nova lista 
 
 
+outracomida = prompt("Qual é sua comida preferida?")
+
+comida[1] = outracomida
+
+console.log("Essas é sua nova lista de comidas preferidas: ")
+console.log(comida[0])
+console.log(comida[1])
+console.log(comida[2])
+console.log(comida[3])
+console.log(comida[4])
