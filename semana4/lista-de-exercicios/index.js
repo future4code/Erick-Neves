e4014f15eaabf9359abf0ce0f3af1cb484290cf4
erickcharlesneves <erick.neves.projetos@gmail.com>
2020-12-11 16:11:34 -0300

    Exercicio 1 - Interpretação de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,3 +1,32 @@
 
+//------------------------EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO----------------------------------
+
+//EXERCICIO 1
+// Temos uma função conversorDeMoeda que recebebe como parâmetro um valor que será utilizado 
+// na função para conversão de moeda, na função é declarada uma variável constante cotação, onde vai 
+// se atribuir o valor digitado atrávez da caixa do prompt esse valor é convertido para Number, em seguida
+// retornamos o valor convertido a partir da operação de multiplicação pelo valor que recebemos do parâmetro
+// pelo valor inserido e atribuido a cotacao, juntamente com a mensagem "R$" antes do valor final da operação
+// declara se outra variável constante meuDinheiro que vai ser atribuida ao valor retornado pela função 
+// conversorDeMoeda, está função usa como parâmetro o valor 100, logo será atribuido a essa constante o valor
+// de 100 * valor inserido para const cotação, portanto será impresso no console o valor final atribuido 
+// a constante meuDinheiro que será o resultado da operação de multiplicação anterior para chegar na conversão da moeda.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //------------------------------------EXERCÍCIOS----------------------------------
 
