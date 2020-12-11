15a386c492defc0d825acd61e1b5094a4c3ddaf4
erickcharlesneves <erick.neves.projetos@gmail.com>
2020-12-11 16:44:52 -0300

    Exercicio 2 - Interpretação de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,7 +1,7 @@
 
 //------------------------EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO----------------------------------
 
-//EXERCICIO 1
+//EXERCICIO 1:
 // Temos uma função conversorDeMoeda que recebebe como parâmetro um valor que será utilizado 
 // na função para conversão de moeda, na função é declarada uma variável constante cotação, onde vai 
 // se atribuir o valor digitado atrávez da caixa do prompt esse valor é convertido para Number, em seguida
@@ -12,7 +12,19 @@
 // de 100 * valor inserido para const cotação, portanto será impresso no console o valor final atribuido 
 // a constante meuDinheiro que será o resultado da operação de multiplicação anterior para chegar na conversão da moeda.
 
-
+//EXERCICIO 2:
+// Temos nesta uma função investeDinheiro que recebe dois parâmetros o tipo e o valor, declara se uma variável
+// alterável let que recebera o valor retornado da operação realizada no caso selecionado atrávez do switch pelo
+// tipo informado no parâmetro tipoDeInvestimento, e o parametro valor informado para que a operação do caso seja 
+// realizada para o seu respectivo caso, no exemplo será chamada a função na primeira constante novoMontante para 
+// atribuir o valor retornado da operação do caso, neste caso será Ações o seu tipo, e 150 o valor, assim a função 
+// executa a operação do caso Ações onde vai ser atribuido na variável constante valorAposInvestimento, a operação
+// do parâmetro definido 150 multiplicado por 1.1, assim o case é parado com break e sua variável passar a ser 
+// retornada com o valor da operação que vai ser 165, para então ser atribuida na nossa constate fora da função 
+// novoMontante a qual chamou a função com os ditos parâmetros, além de ser impresso seu valor no console. 
+//   A mesma lógica acontece com a nossa segunda variável segundoMontante porém com outros parâmetros esses são para
+// o switch default pois o tipo informado não existe, resultando em uma mensagem de caixa alert com a mensagem de 
+// Tipo informado incorreto.
 
 
 
