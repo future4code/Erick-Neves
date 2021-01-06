721947341c170ca4ad6dbc5f35d0f864c1612ad4
erickcharlesneves <erick.neves.projetos@gmail.com>
2021-01-06 02:54:33 -0300

    Lista de tarefas - a22 - Final

--- semana-6/lista-tarefas/src/App.js
+++ semana-6/lista-tarefas/src/App.js
@@ -20,45 +20,58 @@ const InputsContainer = styled.div`
 
 class App extends React.Component {
     state = {
-      tarefas: [
-        {
-          id: Date.now(),
-          texto: 'Texto da primeira tarefa',
-          completa: false // Indica se a tarefa está completa (true ou false)
-        },
-        {
-          id: Date.now(), // Explicação abaixo
-          texto: 'Texto da segunda tarefa',
-          completa: true // Indica se a tarefa está completa (true ou false)
-        }
-    ],
+      tarefas: [],
       inputValue: '',
-      filtro: 'pendentes'
+      filtro: 'nenhum'
     }
 
   componentDidUpdate() {
+    localStorage.setItem("tarefas", JSON.stringify(this.state.tarefas))
 
   };
 
   componentDidMount() {
-
+    const tarefasConstruir = localStorage.getItem("tarefas")
+    if (tarefasConstruir) {
+      this.setState({tarefas: JSON.parse(tarefasConstruir)})
+    }
   };
 
   onChangeInput = (event) => {
+    this.setState({inputValue: event.target.value})
 
   }
 
   criaTarefa = () => {
+    const novaTarefaCriada = {
+      id: Date.now(), 
+      texto: this.state.inputValue, // aqui, o texto da tarefa virá do input controlado guardado no estado
+      completa: false // aqui, pode deixar o valor false para todas as tarefas as serem criadas, pq a tarefa sempre vai começar como não completa.
+    }
 
+    const novosEstadosTarefas = [...this.state.tarefas, novaTarefaCriada]
+
+    this.setState({tarefas: novosEstadosTarefas})
 
   }
 
   selectTarefa = (id) => {
-
-  }
+    const novaListaDeTarefas = this.state.tarefas.map((tarefaa) => {
+      if (id === tarefaa.id) {
+        const novaTarefaCriada = {
+          ...tarefaa,
+          completa: !tarefaa.completa
+        }
+        return novaTarefaCriada
+      } else {
+        return tarefaa
+      }
+  })
+  this.setState({tarefas: novaListaDeTarefas})
+}
 
   onChangeFilter = (event) => {
-
+    this.setState({filtro: event.target.value})
   }
 
   render() {
