99fe1690b45743852057d1fb1272c81709ffc7a8
erickcharlesneves <erick.neves.projetos@gmail.com>
2021-01-13 14:18:43 -0300

    Cadastro de usuários com o primeiro desafio

--- semana-7/A25IntegraAPI/cadastro-usuarios/src/App.js
+++ semana-7/A25IntegraAPI/cadastro-usuarios/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components"
+import { PagCadastro } from "./pages/PagCadastro";
+import { PagListaUsuarios } from "./pages/PagListaUsuarios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BoxApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  border: 1px solid orange;
+  box-shadow: 1px 1px 15px orange;
+  border-radius: 9px;
+`
+const BtnPage = styled.button`
+  margin: 30px;
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  background-color: orangered;
+  color: black;
+  border: none;
+  border-radius: 5px;
+  font-size: 13px;
+  font-weight: 500;
+  text-transform: uppercase;
+  &:hover {
+    background-color: orange;
+  };  
+` 
+
+export default class App extends React.Component {
+  state = {
+    page: true
+  };
+
+  changePage = () => {
+    this.setState({ page: !this.state.page });
+  };
+
+  render() {
+    return (
+      <BoxApp>
+        <h1>Cadastro Labenusers</h1>
+        {this.state.page ? <PagCadastro /> : <PagListaUsuarios />}
+
+        <BtnPage onClick={this.changePage}> {this.state.page ? 'Lista de usuários' : 'Cadastro de usuários'} </BtnPage>
+
+      </BoxApp>
+    );
+  }
 }
-
-export default App;
---
+++ semana-7/A25IntegraAPI/cadastro-usuarios/src/pages/PagListaUsuarios.js
@@ -0,0 +1,100 @@
+import React from "react";
+import axios from 'axios'
+import styled from "styled-components"
+import { baseUrl, axiosConfig } from '../parameters'
+
+const BoxLista = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 70vw;
+    border: 1px solid orangered;
+    box-shadow: 1px 1px 15px orange;
+    border-radius: 9px;
+`
+const BoxListaUser = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 30vw;
+    border: 1px solid orange;
+    box-shadow: 1px 1px 15px orange;
+    border-radius: 9px;
+`
+const ListaUser = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    font-size:3vh;
+    font-weight: 500;
+
+`
+
+const BtnDelete = styled.button`
+  margin-left: 20px;
+  cursor: pointer;
+  padding: 0.3rem 1rem;
+  background-color: red;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 10px;
+  font-weight: 500;
+  
+  &:hover {
+    background-color: orangered;
+  };  
+`
+
+export class PagListaUsuarios extends React.Component {
+    state = {
+        users: []
+    }
+
+    componentDidMount = () => {
+        this.getAllUsers()
+    }
+
+    getAllUsers = () => {
+        axios.get(baseUrl, axiosConfig)
+            .then((response) => {
+                this.setState({ users: response.data })
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    deleteUser = (id) => {
+        axios.delete(`${baseUrl}/${id}`, axiosConfig)
+            .then((response) => {
+                this.getAllUsers()
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    render() {
+
+        return (
+            <BoxLista>
+                <h2>Lista de usuários</h2>
+                {this.state.users.map((user) => {
+                    return (
+                        <BoxListaUser>
+                            <ListaUser>
+                                {user.name}
+                                <BtnDelete onClick={() => { if (window.confirm('Tem certeza que deseja remover este usuário?')) { this.deleteUser(user.id) } }}>X</BtnDelete>
+                            </ListaUser>
+                            
+                        </BoxListaUser>
+                    )
+                })}
+            </BoxLista>
+        );
+    }
+}
